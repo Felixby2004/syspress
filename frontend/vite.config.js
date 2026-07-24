2a13fcf0f50a2bbf34ab2bbc2205ad746c7c882e
Felixby2004 <andree.felix2004@gmail.com>
2026-07-24 12:55:05 -0500

    cambio 1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,18 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public', // Asegura que se copie todo lo de public
   build: {
+    outDir: 'dist',
     rollupOptions: {
       output: {
-        // ...
-      }
-    }
+        // Mantén el nombre de los archivos para caché
+        assetFileNames: 'assets/[name].[hash].[ext]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+      },
+    },
   },
-  publicDir: 'public',
 });
